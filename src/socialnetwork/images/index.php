13a2acded04e637e946c8dc4137c4626e4f87fec
Anjan Nepal <rockanjan@gmail.com>
2012-11-11 04:04:50 -0500

    images added

---
+++ src/socialnetwork/images/index.php
@@ -0,0 +1,9 @@
+<?php
+$page_title = "Register";
+$current_page = "home";
+include_once('../header.php');
+?>
+Nice Try! But you are not allowed to view this page.
+<?php 
+include_once('../footer.php');
+?>
